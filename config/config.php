f49b73ed0be4097c3b351873242485a1009bc83d
Yannick Mahe <mahe.yannick@gmail.com>
2013-05-29 11:51:04 +0200

    conf

--- config/config.php
+++ config/config.php
@@ -3,5 +3,6 @@
 define('TARGET_RATIO',300);
 define('SITE_ROOT','/var/www/selfhostedvideos');//Don't include the trailing slash
 define('SITE_URL','http://url.tld/');//Include the trailing slash
-define('TRANSMISSION_URL','http://yannickmahe.com:9091/transmission/rpc');
+define('TRANSMISSION_URL','http://localhost/transmission/rpc');
+define('NOTIFICATION_RECIPIENT','test@example.org');
 
